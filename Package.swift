a0fab5dc984303055c49fc5ac26c1c3a6fc95870
Ryan Kopinsky <ryan.kopinsky@gmail.com>
2021-09-08 14:50:51 -0500

    Added Flow target with generated code from gRPC and SwiftProtobuf protoc code generation plugins

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FlowSwift",
-            targets: ["FlowSwift"]),
+            targets: ["FlowSwift", "Flow"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,7 +21,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FlowSwift",
-            dependencies: []),
+            dependencies: ["SwiftProtobuf", .product(name: "GRPC", package: "grpc-swift")]),
+        .target(
+            name: "Flow",
+            dependencies: ["SwiftProtobuf", .product(name: "GRPC", package: "grpc-swift")],
+            resources: [
+                .process("README.md"),
+                .copy("Resources")]),
         .testTarget(
             name: "FlowSwiftTests",
             dependencies: ["FlowSwift"]),
